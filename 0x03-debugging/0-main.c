9b7146f59c6b9d9b29037ca8b933d2f45fab8d01
nadaabdelkhalik <isawaynad5@gmail.com>
2023-10-11 12:17:40 -0700

    positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
-/*main -test function to test if the number positive or negative 
- * Return: alwyse 0 success
- */
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
-	i=0;
-	positive_or_negative(i);
-	return(0);
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
