a11009c6bdd8c4540b5bb47242888e5cf892aae8
nadaabdelkhalik <isawaynad5@gmail.com>
2023-10-11 12:16:22 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*main -test function to test if the number positive or negative 
+ * Return: alwyse 0 success
+ */
+
+int main(void)
+{
+	int i;
+	i=0;
+	positive_or_negative(i);
+	return(0);
+}
